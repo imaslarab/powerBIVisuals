d7a6c69fc202a92864601e8208ab6fce6294e304
imaslarab <baralsami@gmail.com>
2016-12-02 14:34:27 +0545

    Scrolling div - unfixed

--- .tmp/drop/visual.prod.js
+++ .tmp/drop/visual.prod.js
@@ -8,30 +8,37 @@ var powerbi;
             (function (PBI_CV_DB82D0E6_E5C1_4E34_884B_CAD22AFB245B) {
                 var Visual = (function () {
                     function Visual(options) {
-                        this.svg = d3.select(options.element).append('svg');
+                        this.container = d3.select(options.element).append('div').style("background", "red").classed('container', true);
+                        // this.container.select('div')
+                        // .append('div').classed('scrollingDiv',true);
+                        this.slider = d3.select('div.container').append('div').classed('slider', true);
+                        this.svg = d3.select('div.slider').append('svg');
                         this.g = this.svg.append('g');
                     }
                     Visual.prototype.update = function (options) {
-                        var _this = this;
                         var width = options.viewport.width, height = options.viewport.height, padding = 100;
-                        _this.svg.attr({
-                            height: height,
+                        d3.select('div.container').attr({
                             width: width
                         });
+                        d3.select('div.slider').attr({
+                            width: width * 4
+                        });
+                        this.container.style("overflow-x", "scroll");
                         var mindate = new Date(2016, 10, 1), maxdate = new Date(2016, 11, 31);
                         var xScale = d3.time.scale()
                             .domain([mindate, maxdate])
-                            .range([padding, width - padding]);
+                            .range([padding, width * 4 - padding]);
                         var xAxis = d3.svg.axis()
                             .orient("bottom")
                             .scale(xScale)
-                            .ticks(d3.time.weeks);
-                        _this.svg.selectAll("*").remove();
-                        _this.svg.append("g")
+                            .ticks(d3.time.weeks)
+                            .tickSize(16, 2);
+                        this.svg.selectAll("*").remove();
+                        this.svg.append("g")
                             .attr("class", "xaxis")
                             .attr("transform", "translate(0," + (height - padding) + ")")
                             .call(xAxis);
-                        _this.svg.selectAll(".xaxis text")
+                        this.svg.selectAll(".xaxis text")
                             .attr("transform", function (d) {
                             return "translate(" + this.getBBox().height * -2 + "," + this.getBBox().height + ")rotate(-45)";
                         });
--- .tmp/precompile/src/visual.ts
+++ .tmp/precompile/src/visual.ts
@@ -1,43 +1,58 @@
 module powerbi.extensibility.visual.PBI_CV_DB82D0E6_E5C1_4E34_884B_CAD22AFB245B  {
 	export class Visual implements IVisual {
 	  private svg: d3.Selection<SVGElement>;
+	  private container: d3.Selection<SVGElement>;
+	  private slider: d3.Selection<SVGElement>;
 	  private g: d3.Selection<SVGElement>;
 
 	  constructor(options: VisualConstructorOptions) {
-			this.svg = d3.select(options.element).append('svg');
+			this.container= d3.select(options.element).append('div').style("background", "red").classed('container',true);
+			// this.container.select('div')
+			// .append('div').classed('scrollingDiv',true);
+			this.slider = d3.select('div.container').append('div').classed('slider', true);
+			this.svg = d3.select('div.slider').append('svg');
 			this.g = this.svg.append('g');
 	  }
 
 	  public update(options: VisualUpdateOptions) {
-			var _this = this;
 			var width = options.viewport.width,
 		      height = options.viewport.height,
 		      padding = 100;
 
-			_this.svg.attr({
-				height: height,
+      d3.select('div.container').attr({
 				width: width
-			});
+			})
 
+		  d3.select('div.slider').attr({
+				width: width*4
+			})
+
+
+
+			this.container.style("overflow-x", "scroll");
+     
       var mindate = new Date(2016,10,1),
       		maxdate = new Date(2016,11,31);
             
       var xScale = d3.time.scale()
 	      .domain([mindate, maxdate]) 
-				.range([padding, width - padding]);
+				.range([padding, width * 4 - padding]);
 
       var xAxis = d3.svg.axis()
         .orient("bottom")
         .scale(xScale)
-        .ticks(d3.time.weeks);
+        .ticks(d3.time.weeks)
+        .tickSize(16, 2)
 
-      _this.svg.selectAll("*").remove();
-	    _this.svg.append("g")
+      this.svg.selectAll("*").remove();
+      
+	    this.svg.append("g")
         .attr("class", "xaxis")   
         .attr("transform", "translate(0," + (height - padding) + ")")
         .call(xAxis);
-       
-      _this.svg.selectAll(".xaxis text")
+
+
+      this.svg.selectAll(".xaxis text")
         .attr("transform", function(d) {
             return "translate(" + this.getBBox().height*-2 + "," + this.getBBox().height + ")rotate(-45)";
       	});	  	
