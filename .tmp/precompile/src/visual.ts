3c635891c90bce60e7c257d55bf84551b2b0cf86
imaslarab <baralsami@gmail.com>
2016-12-08 16:16:04 +0545

    data plotted

--- .tmp/precompile/src/visual.ts
+++ .tmp/precompile/src/visual.ts
@@ -1,8 +1,25 @@
 module powerbi.extensibility.visual.PBI_CV_DB82D0E6_E5C1_4E34_884B_CAD22AFB245B  {
-	
-    function visualTransform(options: VisualUpdateOptions, host: IVisualHost): Date[] {
+	interface TimelineViewModel {
+        dataPoints: TimelineDataPoint[];
+        minDate: Date;
+        maxDate: Date;
+        // settings: TimelineSettings;
+    }
+    interface TimelineDataPoint {
+        title: string;
+        date: Date;
+        // color: string;
+        // selectionId: ISelectionId;
+    };
+
+    function visualTransform(options: VisualUpdateOptions, host: IVisualHost): TimelineViewModel {
         let dataViews = options.dataViews;
-        let viewModel: Date[];
+        let viewModel: TimelineViewModel = {
+            dataPoints: [],
+            minDate: null,
+            maxDate: null
+            // settings: <TimelineSettings>{}
+        };
        
         if (!dataViews
             || !dataViews[0]
@@ -14,15 +31,28 @@ module powerbi.extensibility.visual.PBI_CV_DB82D0E6_E5C1_4E34_884B_CAD22AFB245B 
 						
         let categorical = dataViews[0].categorical;
         let category = categorical.categories[0];
+        let title = categorical.categories[1];
         let dataValue = categorical.values[0];
-        let dataPoints: Date[] = [];
-        var valueInString = category.values[1];
 
-        for (let i = 0, len = Math.max(category.values.length); i < len; i++) {
-            dataPoints.push(new Date(category.values[i].toString()));
+        let timelineDataPoints: TimelineDataPoint[] = [];
+       
+        for (let i in category.values) {
+            timelineDataPoints.push({
+                title: String(title.values[i]),
+                date: new Date(category.values[i].toString())
+            });
         }
-
-        return dataPoints;
+        
+        let dates = timelineDataPoints.map(function(obj){
+            return obj.date;
+        });
+        let maxdate = new Date(Math.max.apply(null,dates));
+        let mindate = new Date(Math.min.apply(null,dates));
+        return {
+            dataPoints: timelineDataPoints,
+            maxDate: maxdate,
+            minDate: mindate
+        };
     }
 
 	export class Visual implements IVisual {
@@ -33,22 +63,19 @@ module powerbi.extensibility.visual.PBI_CV_DB82D0E6_E5C1_4E34_884B_CAD22AFB245B 
         private slider: d3.Selection<SVGElement>;
         private g: d3.Selection<SVGElement>;
 
-		private dates: Date[];
-
         constructor(options: VisualConstructorOptions) {
             this.host = options.host;
-            this.container= d3.select(options.element).append('div').style("background", "red").classed('container',true);
+            this.container= d3.select(options.element).append('div').style("background", "pink").classed('container',true);
             
             this.slider = d3.select('div.container').append('div').classed('slider', true);
             this.svg = d3.select('div.slider').append('svg');
             this.g = this.svg.append('g');
         }
 
-        public update(options: VisualUpdateOptions) {
+        public update(options: VisualUpdateOptions) {           
+            let viewModel: TimelineViewModel = visualTransform(options, this.host);
+            let timelineData = viewModel.dataPoints;
 
-            let viewModel: Date[] = visualTransform(options, this.host);
-            this.dates = viewModel;
-           
             var width = options.viewport.width,
                 height = options.viewport.height,
                 padding = 100;
@@ -65,8 +92,8 @@ module powerbi.extensibility.visual.PBI_CV_DB82D0E6_E5C1_4E34_884B_CAD22AFB245B 
                 width: width
             })
 
-            var maxdate=new Date(Math.max.apply(null,this.dates));
-            var mindate=new Date(Math.min.apply(null,this.dates));
+            var maxdate = viewModel.maxDate;
+            var mindate = viewModel.minDate;
             
             // var mindate = new Date(2016,10,1),
             //     maxdate = new Date(2016,11,31);
@@ -89,12 +116,22 @@ module powerbi.extensibility.visual.PBI_CV_DB82D0E6_E5C1_4E34_884B_CAD22AFB245B 
             .attr("transform", "translate(0," + (height - padding) + ")")
             .call(xAxis);
 
-
             this.svg.selectAll(".xaxis text")
             .attr("transform", function(d) {
                 return "translate(" + this.getBBox().height*-2 + "," + this.getBBox().height + ")rotate(-45)";
-            });	  	
+            });
 
+            let lines = this.svg.selectAll('.line').data(timelineData);
+            lines.enter()
+                .append('line')
+                .classed('line', true);
+            lines.style("stroke", "black");
+            lines.attr({
+                x1: d=> xScale(d.date),
+                y1: height-padding,
+                x2: d=> xScale(d.date),
+                y2: height-padding-100
+            });
         }
 
         public destroy(): void {
